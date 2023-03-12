f44970b5ab7ecad352436506bd95028c3a59a0de
westcorperations <onwusioneejikek04207>
2023-03-12 15:39:10 +0100

    shop with filter done

--- app/Http/Controllers/user/product/UserShopController.php
+++ app/Http/Controllers/user/product/UserShopController.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Products;
 use App\Models\ProductsCategory;
-
+use Illuminate\Database\Query\Builder;
 class UserShopController extends Controller
 {
     //
@@ -14,12 +14,49 @@ class UserShopController extends Controller
     public function index()
     {
         $category = ProductsCategory::all();
-       return view('user.pages.shop',compact('category'));
+        $products = Products::all();
+       return view('user.pages.shop',compact('category','products'));
     }
     public function filter(Request $request)
-{
-    $products = Products::query();
+    {
+     // Get the selected values from the request
 
+     $price_order = $request->input('price_order');
+     $price_range = $request->input('price_range');
+
+
+     // Query the products table with the selected filters
+     $products = Products::query();
+
+
+
+
+     if ($price_order == 1) {
+        // lowest to highest products
+        $products->orderBy('selling_price', 'asc');
+
+
+     }
+     if ($price_order == 2) {
+        // highest to lowest products
+        $products->orderBy('selling_price', 'desc');
+
+
+     }
+
+     if ($price_range) {
+         [$min_price, $max_price] = explode('-', $price_range);
+         $products->whereBetween('selling_price', [$min_price, $max_price]);
+     }
+
+
+     $filtered_products = $products->get();
+     $category = ProductsCategory::all();
+     return view('user.pages.filter',[
+         'products' => $filtered_products,
+         'category' => $category,
+     ]);
+        // return response()->json(['product'=> $filtered_products]);
     // Apply filters
 
 }
